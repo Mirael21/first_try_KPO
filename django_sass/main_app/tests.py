8391535ef49052a6aa8f2c7a44f5bcc2904974f0
Kirill Terekhin <k1r1llter2004@gmail.com>
2023-12-19 01:06:36 +0300

    add get_recommendation

--- django_sass/main_app/tests.py
+++ django_sass/main_app/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+import json
+from . import views
+
 
 # Create your tests here.
+class TestBookRecSys(TestCase):
+    def test_index(self):
+        response = self.client.get("")
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_books_access(self):
+        response = self.client.get("/get_books/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_books_json_correct_data_len(self):
+        response = self.client.get("/get_books/")
+        json_books = json.loads(response.content.decode())
+        json_books = json.loads(json_books)
+        self.assertEquals(len(json_books.keys()), 10)
+
+    def test_get_books_json_correct_data_keys(self):
+        response = self.client.get("/get_books/")
+        json_books = json.loads(response.content.decode())
+        json_books = json.loads(json_books)
+        self.assertEquals(list(json_books['book_0'].keys()), ['title', 'author', 'image', 'description'])
+
+    def test_get_recommendation_access(self):
+        response = self.client.get("/get_recommendation/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_recommendation_json_correct_data_len(self):
+        response = self.client.get("/get_recommendation/")
+        json_books = json.loads(response.content.decode())
+        json_books = json.loads(json_books)
+        self.assertEquals(len(json_books.keys()), 3)
+
+    # def test_get_book_and_marks_from_json(self):
+    #     json_file = {"book_0": {"title": "A", "mark": 1},
+    #                  "book_1": {"title": "B", "mark": 2},
+    #                  "book_2": {"title": "C", "mark": 3},
+    #                  "book_3": {"title": "D", "mark": 4},
+    #                  "book_4": {"title": "E", "mark": 5},
+    #                  "book_5": {"title": "F", "mark": 6},
+    #                  "book_6": {"title": "G", "mark": 7},
+    #                  }
+    #     json_file = json.dumps(json_file)
+    #     books_and_marks = views.get_books_and_marks_from_json(json_file)
+    #     print(type(books_and_marks))
+
+    def test_get_books_by_titles(self):
+        self.assertEquals(views.get_books_by_titles(["The Master and Margarita"]).shape[0], 1)
+
+
--- django_sass/main_app/views.py
+++ django_sass/main_app/views.py
@@ -7,18 +7,28 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 
 
+# @csrf_exempt
+# def save_rating(request):
+#     if request.method == "POST":
+#         data = json.loads(request.body.decode("utf-8"))
+#         print(data)
+#
+#
+#         return JsonResponse({"message": "Rating saved successfully!"})
+#     else:
+#         return JsonResponse({"error": "Invalid request method"})
+
 @csrf_exempt
 def save_rating(request):
     if request.method == "POST":
         data = json.loads(request.body.decode("utf-8"))
-        # book_0 = data.get({"book_0":[{"title" , "rating"}]})
-        # print(book_0)
-
-
-        return JsonResponse({"message": "Rating saved successfully!"})
+        books_and_marks = get_books_and_marks_from_json(data)
+        recommendations = get_recommendation(books_and_marks)
+        print(recommendations)
+        return JsonResponse(recommendations, safe=False)
+        # return JsonResponse({"message": "Rating saved successfully!"})
     else:
         return JsonResponse({"error": "Invalid request method"})
-
 
 def book_page(request):
     return render(request, 'index.html')
@@ -46,29 +56,33 @@ def get_books_by_titles(titles):
     return df_books[df_books['title'].isin(titles)]
 
 
-def get_books_and_marks_from_json(request):
-    pass
+def get_books_and_marks_from_json(data):
+    books_and_marks = {}
+    for i in range(10):
+        books_and_marks[data[f"book_{i}"]['title']] = int(data[f"book_{i}"]['rating'])
+    return books_and_marks
 
 
-def get_recommendation(request):
-    books_and_marks = get_books_and_marks_from_json(request)
-    # df_book_rating_user = pd.read_csv(".\\static\\data\\book_rating_user.csv", index_col=0)
-    # df_user_pivot = (df_book_rating_user.pivot_table(index=["user_id"], columns=["title"], values="rating").fillna(0)
-    #                  .reset_index(drop=True))
-    # user_marks = {i: [0] for i in df_user_pivot.columns}
-    # for title, mark in books_and_marks.items():
-    #     user_marks[title] = [mark]
-    # user_marks = pd.DataFrame(user_marks)
-    # similarity = cosine_similarity(df_user_pivot, user_marks)
-    # sorted_sim = sorted(enumerate(similarity), key=lambda x: x[1], reverse=True)
-    # neighbour_books = set()
-    # for user in sorted_sim:
-    #     for book in df_user_pivot.iloc[user[0]].items():
-    #         if book[1] > 5 and book[0] not in books_and_marks.keys():
-    #             neighbour_books.add(book[0])
-    #         if len(neighbour_books) == 3:
-    #             return get_books_by_titles(neighbour_books)
-    df = pd.read_csv(".\\main_app\\static\\data\\final_books.csv", index_col=0)
-    df_3_books = df.sample(3)
-    df_3_books.reset_index(drop=True, inplace=True)
-    return JsonResponse(make_json(df_3_books), safe=False)
+@csrf_exempt
+def get_recommendation(books_and_marks):
+    df_book_rating_user = pd.read_csv(".\\main_app\\static\\data\\book_rating_user.csv", index_col=0)
+    df_user_pivot = (df_book_rating_user.pivot_table(index=["user_id"], columns=["title"], values="rating").fillna(0)
+                     .reset_index(drop=True))
+    user_marks = {i: [0] for i in df_user_pivot.columns}
+    for title, mark in books_and_marks.items():
+        user_marks[title] = [mark]
+    user_marks = pd.DataFrame(user_marks)
+    similarity = cosine_similarity(df_user_pivot, user_marks)
+    sorted_sim = sorted(enumerate(similarity), key=lambda x: x[1], reverse=True)
+    neighbour_books = set()
+    for user in sorted_sim:
+        for book in df_user_pivot.iloc[user[0]].items():
+            if book[1] > 5 and book[0] not in books_and_marks.keys():
+                neighbour_books.add(book[0])
+            if len(neighbour_books) == 3:
+                return make_json(get_books_by_titles(neighbour_books))
+    # df = pd.read_csv(".\\main_app\\static\\data\\final_books.csv", index_col=0)
+    # df_3_books = df.sample(2)
+    # df_3_books.reset_index(drop=True, inplace=True)
+    # return make_json(df_3_books)
+    # return JsonResponse(make_json(df_3_books), safe=False)
